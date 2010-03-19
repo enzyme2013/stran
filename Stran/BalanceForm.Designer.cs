8173cb1be68957cb0edff3a3af623da83c26f9cd
liuyj3003 <liuyj3003@b81881b8-404d-0410-ae4c-b178b4395a11>
2010-03-19 15:46:31 +0000

    BalanceGrop

---
+++ Stran/BalanceForm.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace Stran
+{
+    partial class BalanceForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textID = new System.Windows.Forms.TextBox();
+            this.textMarket = new System.Windows.Forms.TextBox();
+            this.textMarketTime = new System.Windows.Forms.TextBox();
+            this.textDescription = new System.Windows.Forms.TextBox();
+            this.textReadyTime = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textMinSendResource = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textMaxSendResource = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(71, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "GroupID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(41, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "IgnoreMarket";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(53, 104);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 12);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "IgnoreTime";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(47, 142);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(71, 12);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Description";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(59, 180);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(59, 12);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "ReadyTime";
+            // 
+            // textID
+            // 
+            this.textID.Location = new System.Drawing.Point(143, 28);
+            this.textID.Name = "textID";
+            this.textID.Size = new System.Drawing.Size(100, 21);
+            this.textID.TabIndex = 5;
+            // 
+            // textMarket
+            // 
+            this.textMarket.Location = new System.Drawing.Point(143, 66);
+            this.textMarket.Name = "textMarket";
+            this.textMarket.Size = new System.Drawing.Size(100, 21);
+            this.textMarket.TabIndex = 6;
+            // 
+            // textMarketTime
+            // 
+            this.textMarketTime.Location = new System.Drawing.Point(143, 106);
+            this.textMarketTime.Name = "textMarketTime";
+            this.textMarketTime.Size = new System.Drawing.Size(100, 21);
+            this.textMarketTime.TabIndex = 7;
+            // 
+            // textDescription
+            // 
+            this.textDescription.Location = new System.Drawing.Point(143, 145);
+            this.textDescription.Name = "textDescription";
+            this.textDescription.Size = new System.Drawing.Size(100, 21);
+            this.textDescription.TabIndex = 8;
+            // 
+            // textReadyTime
+            // 
+            this.textReadyTime.Location = new System.Drawing.Point(143, 184);
+            this.textReadyTime.Name = "textReadyTime";
+            this.textReadyTime.Size = new System.Drawing.Size(100, 21);
+            this.textReadyTime.TabIndex = 9;
+            // 
+            // button1
+            // 
+            this.button1.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.button1.Location = new System.Drawing.Point(43, 297);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "OK";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.button2.Location = new System.Drawing.Point(143, 297);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Cancle";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(23, 218);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(95, 12);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "MinSendResource";
+            // 
+            // textMinSendResource
+            // 
+            this.textMinSendResource.Location = new System.Drawing.Point(143, 218);
+            this.textMinSendResource.Name = "textMinSendResource";
+            this.textMinSendResource.Size = new System.Drawing.Size(100, 21);
+            this.textMinSendResource.TabIndex = 13;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(23, 261);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(95, 12);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "MaxSendResource";
+            // 
+            // textMaxSendSource
+            // 
+            this.textMaxSendResource.Location = new System.Drawing.Point(143, 261);
+            this.textMaxSendResource.Name = "textMaxSendSource";
+            this.textMaxSendResource.Size = new System.Drawing.Size(100, 21);
+            this.textMaxSendResource.TabIndex = 15;
+            // 
+            // BalanceForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(304, 362);
+            this.Controls.Add(this.textMaxSendResource);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textMinSendResource);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textReadyTime);
+            this.Controls.Add(this.textDescription);
+            this.Controls.Add(this.textMarketTime);
+            this.Controls.Add(this.textMarket);
+            this.Controls.Add(this.textID);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "BalanceForm";
+            this.Text = "BalanceForm";
+            this.Load += new System.EventHandler(this.BalanceForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textID;
+        private System.Windows.Forms.TextBox textMarket;
+        private System.Windows.Forms.TextBox textMarketTime;
+        private System.Windows.Forms.TextBox textDescription;
+        private System.Windows.Forms.TextBox textReadyTime;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textMinSendResource;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textMaxSendResource;
+    }
+}
---
+++ Stran/BalanceForm.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using libTravian;
+
+namespace Stran
+{
+    public partial class BalanceForm : Form
+    {
+        #region Fields
+        private TextBox[] txt;
+        #endregion
+
+        #region Properties
+        public TBalancerGroup BalancerGroup { get; set; }
+        public TVillage Village { get; set; }
+        public MUI mui { get; set; }
+        public Travian UpCall { get; set; }
+        #endregion
+
+        public BalanceForm()
+        {
+            InitializeComponent();
+        }
+
+        private void BalanceForm_Load(object sender, EventArgs e)
+        {
+            if (BalancerGroup == null)
+            {
+                BalancerGroup = TBalancerGroup.GetDefaultTBalancerGroup();
+            }
+            this.textID.Text = BalancerGroup.ID.ToString();
+            this.textMarket.Text = ((int)BalancerGroup.IgnoreMarket).ToString();
+            this.textMarketTime.Text = BalancerGroup.IgnoreMarketTime.ToString();
+            this.textDescription.Text = BalancerGroup.desciption;
+            this.textReadyTime.Text = BalancerGroup.ReadyTime.ToString();
+            this.textMinSendResource.Text = BalancerGroup.MinSendResource.ToString();
+            this.textMaxSendResource.Text = BalancerGroup.MaxSendResource.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            TBalancerGroup group = new TBalancerGroup();
+            group.ID = Convert.ToInt32(this.textID.Text);
+             
+            if (Convert.ToInt32(this.textMarket.Text) == 1)
+            {
+                group.IgnoreMarket = BalancerQueue.IgnorMarketType.ignore;
+            }
+            else
+            {
+                group.IgnoreMarket = BalancerQueue.IgnorMarketType.notignore;
+            }
+                
+            group.IgnoreMarketTime = Convert.ToInt32(this.textMarketTime.Text);
+            group.desciption = this.textDescription.Text;
+            group.ReadyTime = Convert.ToInt32(this.textReadyTime.Text);
+
+            group.MinSendResource = Convert.ToInt32(textMinSendResource.Text);
+            group.MinSendResource = Convert.ToInt32(textMinSendResource.Text);
+
+            BalancerGroup = group;
+
+            
+        }
+    }
+}
